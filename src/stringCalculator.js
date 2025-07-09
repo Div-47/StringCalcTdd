72ff65a41193ce1db68dbca68b4140115750984a
Divyanshu <dbisht141@gmail.com>
2025-07-09 14:40:10 +0530

    string calc functionality and test added for itself a string one number

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,13 @@
 function add(numbers) {
   if (!numbers) return 0;
+  const tokens = numbers.split("");
+  let result = 0;
+  for (let token of tokens) {
+    if (token === '') continue;
+    const num = parseInt(token);
+    if (num >0) result += num;
+  }
+  return result;
 }
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,8 @@ describe('String Calculator TDD Kata', () => {
   test('returns 0 for empty string', () => {
     expect(add("")).toBe(0);
   });
+
+  test('returns number itself for one number', () => {
+    expect(add("1")).toBe(1);
+  });
 });
