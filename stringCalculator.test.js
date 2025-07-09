d8297f5557d1b3507b00c71663d9294a3b27a74a
Divyanshu <dbisht141@gmail.com>
2025-07-09 14:43:57 +0530

    test added for adding multiple numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -12,4 +12,10 @@ describe('String Calculator TDD Kata', () => {
   test('adds two numbers', () => {
     expect(add("1,2")).toBe(3);
   });
+
+  test('adds multiple numbers', () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+  });
+
+  
 });
